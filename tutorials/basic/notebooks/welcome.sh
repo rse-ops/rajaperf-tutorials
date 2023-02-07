f2df5194413060ab18e62b5f3dfceda42855b8bf
holger <jonesholger@gmail.com>
2023-02-07 11:59:48 -0800

    revert to most simple jupyter config using some artifacts copied from the flux tutorial; this is baseline before we turn spack builds back on

--- tutorials/basic/notebooks/welcome.sh
+++ tutorials/basic/notebooks/welcome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "🌀️ Welcome to the Flux Framework RADIUSS Tutorial! 🌀️"
+echo "🌀️ Welcome to the RAJAPerf Framework RADIUSS Tutorial! 🌀️"
 
 
 echo "If you are running this locally (and can see this message)"
@@ -8,7 +8,7 @@ echo "You can open your browser to https://127.0.0.1."
 echo "We use self-signed certificates, so you can proceed."
 printf "Your login information is:\n\n"
 
-echo "🥑️ user: fluxuser"
+echo "🥑️ user: ubuntu"
 printf "🥑️ password: ${password}\n\n"
 
 echo "Have fun! ⭐️🦄️⭐️"
